f6418f4b4c592268d0bd6ff27717f85dc4e51608
janrosantos <janrosantos@gmail.com>
2018-02-07 17:19:21 +0800

    Main test update

--- DMT_POC/src/com/mapping/xmlcustomizer/testing/XMLCustomizerTester.java
+++ DMT_POC/src/com/mapping/xmlcustomizer/testing/XMLCustomizerTester.java
@@ -10,10 +10,13 @@ public class XMLCustomizerTester {
 	public static void main(String[] args) {
 
 	    try {
-	      TransformationInput in = new FileInputStream(new File("in.xml"));
-	      OutputStream out = new FileOutputStream(new File("out.xml"));
+	      FileInputStream in = new FileInputStream(new File("in.xml"));
+	      FileOutputStream out = new FileOutputStream(new File("out.xml"));
 	      XMLCustomizer myMapping = new XMLCustomizer();
-	      myMapping.transform(in, out);
+//	      myMapping.transform(in, out);
+	      
+	      out.write(in.read());
+	      
 	    } catch (Exception e) {
 	      e.printStackTrace();
 	    }
